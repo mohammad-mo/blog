a066acddfd99b40b55b67b35793d1fc7278040a8
Mohammad <mohammadmahone@gmail.com>
2022-06-05 10:18:28 +0430

    - Minor changes.

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -3,7 +3,7 @@ import { FaMicroblog, FaSearch } from 'react-icons/fa'
 const Navbar = () => {
   return (
     <div className='w-full h-12 sticky top-0 flex items-center bg-white z-30'>
-      <div className='flex justify-center items-center space-x-1 w-1/4 h-12 text-xl'>
+      <div className='flex justify-start items-center space-x-1 w-1/4 h-12 text-xl'>
         <p>Blog</p> <FaMicroblog />
       </div>
       <div className='w-2/3'>
@@ -15,7 +15,7 @@ const Navbar = () => {
           <li className='cursor-pointer'>LOGOUT</li>
         </ul>
       </div>
-      <div className='flex justify-center items-center space-x-2 w-1/4 h-12'>
+      <div className='flex justify-end items-center space-x-2 w-1/4 h-12'>
         <div className='bg-blue-400 w-8 h-8 rounded-full'></div>
         <FaSearch />
       </div>
