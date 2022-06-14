0113a5d67ad39d09b229b82ae07498ca31ccf854
Mohammad <mohammadmahone@gmail.com>
2022-06-14 11:06:41 +0430

    - Enable text compression.

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const { errorHandler } = require('./middlewares/error')
 const connectDB = require('./config/db')
 const path = require('path')
 const cors = require('cors')
+const compression = require('compression')
 const PORT = process.env.PORT
 
 // Connect to database
@@ -12,6 +13,7 @@ connectDB()
 
 const app = express()
 
+app.use(compression())
 app.use(cors())
 app.use(express.json({ limit: '5mb' }))
 app.use(express.urlencoded({ limit: '5mb', extended: true }))
